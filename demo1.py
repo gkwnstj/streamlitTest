59540de44a95f451856d5d670a7bccb8250695f3
gkwnstj <87974851+gkwnstj@users.noreply.github.com>
2023-06-14 20:20:57 +0900

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,5 +1,5 @@
 import easyocr
-reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
-result = reader.readtext('nobrand.jpg')
+reader = easyocr.Reader(['en','ko']) # this needs to run only once to load the model into memory
+result = reader.readtext('nobrand.jpg', detail = 0, paragraph=True)
 
 print(result)
