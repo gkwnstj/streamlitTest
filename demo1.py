f37301d8d63138efc14782a07fa9fd0d40935bcb
gkwnstj <87974851+gkwnstj@users.noreply.github.com>
2023-06-14 20:28:03 +0900

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,5 +1,6 @@
 import easyocr
+import streamlit as st
 reader = easyocr.Reader(['en','ko']) # this needs to run only once to load the model into memory
 result = reader.readtext('nobrand.jpg', detail = 0, paragraph=True)
 
-print(result)
+st.text(result)
