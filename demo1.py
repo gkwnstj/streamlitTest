965551134ad1145a3331ebd879c29b29b4f27183
gkwnstj <87974851+gkwnstj@users.noreply.github.com>
2023-06-26 21:28:16 +0900

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -38,29 +38,29 @@ def process_image(file, tesseract_lang):   # we change the format of the languag
     st.text(text)
     return text
 
-def process_speech():
-    r = sr.Recognizer()
-    audio = ""
-    with sr.Microphone() as source:
-        st.text("무엇을 도와드릴까요?")
-        try:
-            speech = r.listen(source)
-            time.sleep(5)
-            #noise 제거
-            r.adjust_for_ambient_noise(source)
-        except sr.WaitTimeoutError:
-            st.text("Timeout error occurred")
-            return ""
+# def process_speech():
+#     r = sr.Recognizer()
+#     audio = ""
+#     with sr.Microphone() as source:
+#         st.text("무엇을 도와드릴까요?")
+#         try:
+#             speech = r.listen(source)
+#             time.sleep(5)
+#             #noise 제거
+#             r.adjust_for_ambient_noise(source)
+#         except sr.WaitTimeoutError:
+#             st.text("Timeout error occurred")
+#             return ""
 
-    try:    
-        audio = r.recognize_google(speech, language="ko-KR")
-        st.text("Your speech thinks like\n " + audio)
+#     try:    
+#         audio = r.recognize_google(speech, language="ko-KR")
+#         st.text("Your speech thinks like\n " + audio)
         
-    except sr.UnknownValueError:
-        st.text("Your speech can not understand")
-    except sr.RequestError as e:
-        st.text("Request Error!; {0}".format(e))
-    return audio
+#     except sr.UnknownValueError:
+#         st.text("Your speech can not understand")
+#     except sr.RequestError as e:
+#         st.text("Request Error!; {0}".format(e))
+#     return audio
 
 
 def process_speech():
@@ -128,6 +128,6 @@ if uploaded_file is not None:
 if st.checkbox('Process Speech'):
     st.session_state.audio_text = process_speech()
 
-if st.checkbox('Process ChaGPT_API'):
+if st.checkbox('Process ChatGPT_API'):
     process_chatgpt(st.session_state.audio_text, st.session_state.image_text)
 
